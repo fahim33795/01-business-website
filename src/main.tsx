ff6902ba47bbddb523e32d496164344e942fe186
fahim33795 <owillson9@gmail.com>
2026-09-16 09:47:03 +0600

    Explicitly declare props in ErrorBoundary to fix TypeScript error

--- src/main.tsx
+++ src/main.tsx
@@ -13,10 +13,17 @@ interface ErrorBoundaryState {
 }
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  declare props: ErrorBoundaryProps;
+
   public state: ErrorBoundaryState = {
     hasError: false,
     error: null
   };
+
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.props = props;
+  }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
     return { hasError: true, error };
